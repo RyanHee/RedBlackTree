6cd54c3a6ba9913e2f0ecf3c7e520c6703ba2ccf
Ryan <111530605+RyanHee@users.noreply.github.com>
2024-02-13 18:02:27 -0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        RedBlackTree tree = new RedBlackTree();
+
+        System.out.println("enter command");
+        Scanner sc = new Scanner(System.in);
+        //sc.nextLine();
+        String s = sc.nextLine();
+        String[] lst = s.split(" ");
+        if (lst[0].equals("insert")){
+            //System.out.println(true);
+            for (int i=1;i<lst.length;i++){
+                tree.add(new RedBlackNode(Integer.parseInt(lst[i])));
+                System.out.println(tree.fullLevelOrder());
+            }
+        }
+
+        System.out.println(tree.fullLevelOrder());
+        System.out.println(Arrays.toString(tree.testFind(9)));
+        System.out.println(Arrays.toString(tree.testFind(46)));
     }
 }
